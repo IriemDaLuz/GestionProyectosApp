1b1f16f2f550184d974514f73effb003bf6ce6d5
IriemDaLuz <yenduuca2005@gmail.com>
2025-01-30 12:36:42 +0000

    ProyectoScreen

--- src/main/kotlin/ProyectoScreen.kt
+++ src/main/kotlin/ProyectoScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.TopCenter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -16,21 +17,32 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 
 
-class ProyectoScreen(private val projectName: String) : Screen {
+class ProyectoScreen(private val nombreProyecto: String) : Screen {
     @Composable
     override fun Content() {
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Color(0xFF121212)),
-            contentAlignment = Alignment.Center
+            contentAlignment = TopCenter
         ) {
-            Text(
-                text = "Detalles de $projectName",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
+            Row (verticalAlignment = Alignment.CenterVertically){
+                Text(
+                    text = "Detalles de ",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+                Text(
+                    text = nombreProyecto,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Cyan
+                )
+            }
+
+
         }
     }
 }
